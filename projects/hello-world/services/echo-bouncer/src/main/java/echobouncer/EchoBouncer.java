52e606216d6a8ed5d8abd8b18f99aaa112414c3e
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2021-03-16 21:54:32 +0100

    make checkstyle happy

--- projects/hello-world/services/echo-bouncer/src/main/java/echobouncer/EchoBouncer.java
+++ projects/hello-world/services/echo-bouncer/src/main/java/echobouncer/EchoBouncer.java
@@ -1,29 +1,32 @@
+/*
+ *
+ */
 package echobouncer;
 
 import com.github.slamdev.openapispringgenerator.plugin.api.RequestDto;
 import com.github.slamdev.openapispringgenerator.plugin.api.ResponseDto;
-import lombok.extern.slf4j.Slf4j;
+import com.github.slamdev.openapispringgenerator.plugin.api.TestingApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.github.slamdev.openapispringgenerator.plugin.api.TestingApi;
 
 import java.util.List;
 
-@Slf4j
 @SpringBootApplication
-public class EchoBouncer implements TestingApi {
+public final class EchoBouncer implements TestingApi {
 
-    public EchoBouncer() {
-        log.info("{}", "asd");
-    }
-
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(EchoBouncer.class);
+    /**
+     * App entrypoint.
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        SpringApplication application = new SpringApplication(
+                EchoBouncer.class);
         application.run(args);
     }
 
     @Override
-    public List<ResponseDto> exec(List<RequestDto> body) {
+    public List<ResponseDto> exec(final List<RequestDto> body) {
         return null;
     }
 }
---
+++ projects/hello-world/services/echo-bouncer/src/main/java/echobouncer/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Java package.
+ */
+package echobouncer;
